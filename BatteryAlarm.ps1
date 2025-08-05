3acd5a6b2f6132081b3e4f509b2b62fd7be0d2eb
gfrescari <134837973+gfrescari@users.noreply.github.com>
2025-08-05 23:43:57 +0700

    Update BatteryAlarm.ps1
    Refined the code

--- BatteryAlarm.ps1
+++ BatteryAlarm.ps1
@@ -1,13 +1,23 @@
+Clear-Host
+Write-Host "---------------------------------------------"
+Write-Host "Battery Alarm Check Every 5 Minutes" -ForegroundColor Cyan
+Write-Host "---------------------------------------------"
+
+
 $sp = New-Object -ComObject SAPI.SpVoice
-while(1){$batt=(Get-WmiObject win32_battery).estimatedChargeRemaining; `
-if ($batt -gt 94){ `
-$sp.Speak("Battery almost full at $batt%");$sp.Speak("Battery almost full at $batt%");$sp.Speak("Battery almost full at $batt%"); `
-} `
-elseif($batt -lt 25){ `
-$sp.Speak("Battery low at $batt%");$sp.Speak("Battery low at $batt%");$sp.Speak("Battery low at $batt%"); `
-} `
-elseif($batt -gt 47 -and $batt -lt 53){ `
-$sp.Speak("Battery at $batt%");$sp.Speak("Battery at $batt%");$sp.Speak("Battery at $batt%"); `
-} `
-Start-Sleep -Seconds 300 `
+while(1){
+    $batt=(Get-WmiObject win32_battery).estimatedChargeRemaining;
+    if ($batt -gt 90){
+        for ($i=0;$i -le 2;$i++){$sp.Speak("Battery almost full at $batt%") | Out-Null}
+        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+    }
+    elseif($batt -lt 25){
+        for ($i=0;$i -le 2;$i++){$sp.Speak("Battery low at $batt%") | Out-Null}
+        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+    }
+    elseif($batt -gt 47 -and $batt -lt 53){
+        for ($i=0;$i -le 2;$i++){$sp.Speak("Battery at $batt%") | Out-Null}
+        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+    }
+    Start-Sleep -Seconds 300
 }
