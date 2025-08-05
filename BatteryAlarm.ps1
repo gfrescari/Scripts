01f6541a49c29a7fcd26e9a7659e5fb6fa92c602
gfrescari <134837973+gfrescari@users.noreply.github.com>
2025-08-05 23:50:40 +0700

    Update BatteryAlarm.ps1
    added percentage to the battery level

--- BatteryAlarm.ps1
+++ BatteryAlarm.ps1
@@ -9,15 +9,16 @@ while(1){
     $batt=(Get-WmiObject win32_battery).estimatedChargeRemaining;
     if ($batt -gt 90){
         for ($i=0;$i -le 2;$i++){$sp.Speak("Battery almost full at $batt%") | Out-Null}
-        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+        Write-Host "Battery was $batt% at $(Get-Date -Format 'HH:mm:ss')"
     }
     elseif($batt -lt 25){
         for ($i=0;$i -le 2;$i++){$sp.Speak("Battery low at $batt%") | Out-Null}
-        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+        Write-Host "Battery was $batt% at $(Get-Date -Format 'HH:mm:ss')"
     }
     elseif($batt -gt 47 -and $batt -lt 53){
         for ($i=0;$i -le 2;$i++){$sp.Speak("Battery at $batt%") | Out-Null}
-        Write-Host "Battery was $batt at $(Get-Date -Format 'HH:mm:ss')"
+        Write-Host "Battery was $batt% at $(Get-Date -Format 'HH:mm:ss')"
     }
     Start-Sleep -Seconds 300
 }
+
