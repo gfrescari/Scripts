babd14ffa3f1128fb8484ae7160eb979157accc4
gfrescari <134837973+gfrescari@users.noreply.github.com>
2025-09-15 22:28:09 +0700

    Create batterymonitor.py
    The script that is running on the device A to check time and battery level of the laptop

---
+++ ScriptsOverTCP/batterymonitor.py
@@ -0,0 +1,28 @@
+import psutil
+import time
+import sys
+from datetime import datetime
+
+def batterymonitor():
+    while True:
+        battery = psutil.sensors_battery()
+        if battery:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            isCharging = 'Charging' if battery.power_plugged else 'Not Charged'
+            print(f"Time: {current_time}")
+            sys.stdout.flush()
+            print(f"Battery: {battery.percent}% {isCharging}")
+            sys.stdout.flush()
+            
+        else:
+            print(f"Battery info not available")
+        time.sleep(60)
+        for _ in range(2):
+            sys.stdout.write("\033[F")
+            sys.stdout.flush()
+            sys.stdout.write("\033[K")
+            sys.stdout.flush()
+            
+if __name__ == "__main__":
+    batterymonitor()
