0a1e627c831d4e0f064166b47fbb87495734170a
gfrescari <134837973+gfrescari@users.noreply.github.com>
2025-09-15 22:32:02 +0700

    Create outputovertcp.py
    the script that run and send the output of the bat file mentioned before. Run this in device A

---
+++ ScriptsOverTCP/outputovertcp.py
@@ -0,0 +1,21 @@
+import socket
+import subprocess
+
+ANDROID_IP = '192.168.x.x'  # Replace with your Android IP
+ANDROID_PORT = 8022
+
+def send_bat_output():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ANDROID_IP, ANDROID_PORT))
+        process = subprocess.Popen(['cmd.exe', '/c', 'batterymonitor.bat'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True,bufsize=1)
+
+        for line in iter(process.stdout.readline, ''):
+            if not line:
+                break
+            s.sendall(line.encode())
+
+        process.stdout.close()
+        process.wait()
+
+if __name__ == '__main__':
+    send_bat_output()
